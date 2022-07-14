474aa99fa5e6ee7cbe277a15330682b34c651862
msmx <maximilian.selmayr@gmx.de>
2022-07-14 11:21:42 +0200

    123

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -13,7 +13,17 @@ public class QueryProcessor {
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
            return "Maxi";
-        } else { // TODO extend the programm here
+        } else if (query.contains("plus")){
+            String[] temp = query.split(" ");
+            int i= Integer.parseInt(temp[2]) + Integer.parseInt(temp[4]);
+            return String.valueOf(i);
+        } else if (query.contains("which of the following numbers is the largest")){
+            String[] temp = query.split(" ");
+            temp[8].replace(",", "");
+            if(Integer.parseInt(temp[8])>Integer.parseInt(temp[9]))return temp[8];
+            else return temp[9];
+        }
+        else { // TODO extend the programm here
             return "";
         }
     }
